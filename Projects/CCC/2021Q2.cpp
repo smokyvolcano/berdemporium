8e83febab20896e76685047bdfc3430d35c2b70f
smokyvolcano <joeyliu128@gmail.com>
2021-05-30 19:27:11 -0700

    Save 1

--- Projects/CCC/2021Q2.cpp
+++ Projects/CCC/2021Q2.cpp
@@ -1,9 +1,28 @@
+//https://cemc.math.uwaterloo.ca/contests/computing/2021/ccc/juniorEF.pdf
+
 #include <iostream>
 using namespace std;
 
 int main(){
 
-	int n;
+	cin >> numberOfBids;
+
+	int numberOfBids; 
+	string people [numberOfBids];
+	string amount [numberOfBids];
+
+	int personNumber;
+	int amountNumber;
+
+for {i=0; i<= numberOfBids; i++;){
+	cin >> people [i];
+	cin >> amount [i];
+}
+
+
+
+	
+
 
 	
 	return 0;
