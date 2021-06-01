0bbd002e7a2b3f8e72487a34b68e7ee298223978
smokyvolcano <joeyliu128@gmail.com>
2021-05-31 20:46:59 -0700

    Deleted all exe and new reversing_a_string file

--- Learning C++/char_arrays.cpp
+++ Learning C++/char_arrays.cpp
@@ -6,8 +6,23 @@ int main() {
 	char text[] = "hello";
 
 	for(int i=0; i<sizeof(text); i++) {
-		cout << i << ": " << text[i] << flush;
+		cout << i << ": " << text[i] << endl;
 	}
+
+	int k=0;
+
+	while(true){
+
+		if (text[k] == 0) {
+			break;
+		}
+
+		cout << text[k] << flush;
+
+		k++;
+	}
+
+	
 
 	return 0;
 }
---
+++ Learning C++/reversing_a_string.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	char text[] = "hello";
+
+	int nChars = sizeof(text)-1; //number of characters in the string
+
+	char *pStart = text;
+	char *pEnd = text + nChars - 1;
+
+	while(pStart < pEnd) {
+
+		char save = *pStart;
+		*pStart = *pEnd;
+		*pEnd = save;
+
+		pStart++;
+		pEnd--;
+	}
+
+	cout << text << endl;
+
+	return 0;
+}
