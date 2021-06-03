8cf1e0ffd69b458437c54dae0d9f64438cd63b2c
smokyvolcano <joeyliu128@gmail.com>
2021-06-03 16:34:56 -0700

    New file folder

---
+++ Learning C++/Constructors_and_Destructors/Cat.h
@@ -0,0 +1,8 @@
+class Cat{
+private:
+	bool happy;
+
+public:
+	void speak();
+};
+
---
+++ Learning C++/Constructors_and_Destructors/cat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+void Cat::speak(){
+	if(happy){
+		cout << "I am a happy fat cat" << endl;
+		}
+	else {
+		cout << "I am not a happy fat cat" << endl;
+	}
+
+}
+
+int main(){
+
+}
