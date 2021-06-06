5ca8acf8cd3668253078439d3d92909c95f07e12
smokyvolcano <joeyliu128@gmail.com>
2021-06-05 18:10:40 -0700

    Created new returning_objects_from_functions

---
+++ Learning C++/returning_objects_from_functions.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Animal{
+private:
+	string name;
+
+public:
+	Animal(){
+		cout << "Animal created." << endl;
+
+	}
+
+	~Animal(){
+		cout << "Destructor called" << endl;
+	}
+
+	void setName(string name){
+		this->name = name;
+	}
+
+	void speak() const{
+		cout << "My name is: " << name << endl;
+	}
+};
+
+Animal *createAnimal() {
+	Animal *pAnimal = new Animal();
+	pAnimal->setName("Bertie");
+	return pAnimal;
+}
+
+
+int main() {
+	Animal *pFrog = createAnimal();
+
+	pFrog->speak();
+
+	delete pFrog;
+
+	}
