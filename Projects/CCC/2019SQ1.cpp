c56376bc983c8ad940da43a5266ef162190579aa
smokyvolcano <joeyliu128@gmail.com>
2021-06-06 14:06:36 -0700

    Bug Fixes to SQ1.cpp

--- Projects/CCC/2019SQ1.cpp
+++ Projects/CCC/2019SQ1.cpp
@@ -4,29 +4,39 @@ using namespace std;
 
 int main(){
 
-	char H = "H";
-	char V = "V";
 
 	int t1 = 1;
 	int t2 = 2;
 	int b1 = 3;
 	int b2 = 4;
 	int standard[2][2] = {{t1, t2}, {b1, b2}};
-	cout << t1 << " " << t2 << endl;
-	cout << b1 << " " << b2 << endl;
+	// cout << t1 << " " << t2 << endl;
+	// cout << b1 << " " << b2 << endl;
 
 
 	string input;
 	cin >> input;
 
-	int n = input.length();
-	
-	char input_array[n+1];
-	strcpy(input_array, input.c_str());
-	//char array now = the string of HV
+	char* input_arr;
+	string str_obj = input;
+	input_arr = &str_obj[0];
+	// cout << input_arr[0];
 
-	for (int i; i<n; i++){
-		if (input_array[i] == H){
+	int n = sizeof(input_arr)/sizeof(char);
+
+	// int n = input.length();
+	// char input_array[n+1]; // const char*
+	// strcpy(input_array, input.c_str());
+
+	// for (int i = 0; i<n; i++){
+	// 	cout << input_array[i] << endl;;
+	// }
+
+	for (int i = 0; i<n+1; i++){
+	
+		// cout << "Bug Test" << " " << i << endl;
+
+		if (input_arr[i] == 'H'){ 
 			int s1 = t1;
 			t1 = t2;
 			t2 = s1;
@@ -35,7 +45,7 @@ int main(){
 			b2 = s2;
 		}
 
-		if (input_array[i] == V){
+		if (input_arr[i] == 'V'){
 			int s1 = t1;
 			t1 = b1;
 			b1 = s1;
@@ -45,6 +55,9 @@ int main(){
 		}
 
 	}
+
+	cout << t1 << " " << t2 << endl;
+	cout << b1 << " " << b2 << endl;
 
 
 	return 0;
